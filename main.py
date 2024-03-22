8cef8a34d67dab232a9e5487a91f2365a35ff52a
heislera <heislera@msoe.edu>
2024-03-22 15:53:36 -0500

    test normalization of review data; added csv files in local directory

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pandas as pd
+import re
 def main() -> None:
-    print("Hello World")
+
+    df = pd.read_csv("data/rotten_tomatoes_critic_reviews.csv")
+    print(df)
+    df['review_content'] = df['review_content'].apply(lambda x: re.sub(r'[^\w\s]', '', str(x).lower()))
+    print(df)
 
 
 if __name__ == '__main__':
