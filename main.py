e7c0d51dc1c5fc3a0f31759a0d0cd8114bb8efe6
hartleyv <hartleyv@msoe.edu>
2024-03-28 10:00:34 -0500

    small changes

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pandas as pd
-import re
-def main() -> None:
 
-    df = pd.read_csv("data/rotten_tomatoes_critic_reviews.csv")
-    print(df)
-    df['review_content'] = df['review_content'].apply(lambda x: re.sub(r'[^\w\s]', '', str(x).lower()))
-    print(df)
+def load_data():
+    critic_reviews = pd.read_csv("data/rotten_tomatoes_critic_reviews.csv")
+    movie_data = pd.read_csv("data/rotten_tomatoes_movies.csv")
+    return critic_reviews, movie_data
+
+def main() -> None:
+    critic_reviews, movie_data = load_data()
+
+    # remove nulls from review content
+    critic_reviews = critic_reviews[critic_reviews['review_content'].notnull()]
+
+    # set review content to lowercase
+    critic_reviews['review_content'] = critic_reviews['review_content'].apply(lambda x: str(x).lower())
+
+    temp = critic_reviews['review_content'].apply(lambda x: x.split())
+
 
 
 if __name__ == '__main__':
